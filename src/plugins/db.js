48bc51a0a869200245ebc8f53ee0edf29fcc5f64
SagarSensation <105171869+lionelraykai@users.noreply.github.com>
2025-06-13 15:53:32 +0530

    Fix mongoURI DB passed as string

--- src/plugins/db.js
+++ src/plugins/db.js
@@ -5,18 +5,14 @@ require("dotenv").config();
 async function dbConnector(fastify, options) {
   try {
     fastify.log.info("Connecting to MongoDB...");
-    await mongoose.connect("mongodb+srv://TrainingTrace:2r8bIl867n0J6lOU@trainingtrace.w3mkfb0.mongodb.net/Trainingtrace", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect("mongodb+srv://TrainingTrace:2r8bIl867n0J6lOU@trainingtrace.w3mkfb0.mongodb.net/");
 
     fastify.log.info("MongoDB connected");
 
-    // Decorate fastify instance with mongoose so it's accessible elsewhere
     fastify.decorate("mongoose", mongoose);
   } catch (err) {
     fastify.log.error("MongoDB connection failed", err);
-    throw err; // This is important: let Fastify handle the failure
+    throw err; 
   }
 }
 
