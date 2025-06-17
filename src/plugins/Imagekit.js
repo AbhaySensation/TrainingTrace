f9a1f0a3be0e5622943d15687b0f33c51456b313
SagarSensation <105171869+lionelraykai@users.noreply.github.com>
2025-06-17 11:02:16 +0530

    updated imagekit keys

--- src/plugins/Imagekit.js
+++ src/plugins/Imagekit.js
@@ -1,8 +1,8 @@
 const  ImageKit = require("imagekit");
 const  imagekit = new ImageKit({
-  publicKey: "public_IVy4jNJzFkigNdxzul41ASSVNmY=",
-  privateKey: "private_pahWa3G6zD******************",
-  urlEndpoint: "https://ik.imagekit.io/liajsi8ud",
+  publicKey: "public_wD3A4ZzdJ+TKxE/Njap0L6DzCNU=",
+  privateKey: "private_0oyNYQDBAVj1Mg2NdLLucDfVAZ4=",
+  urlEndpoint: "https://ik.imagekit.io/9gkfcpjkm",
 });
 
 module.exports = imagekit;
